92c824e081018520b1c70d8175312a23c861a91a
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 08:21:58 +0000

    Item list test set up, test fails

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -14,5 +14,14 @@ end
     it 'prints out the instruction message' do
       expect(subject.instructions).to eq("To buy a snack, please insert some coins and then confirm the the item code!🪙🪙🪙")
     end
-  end
+	end
+
+	  describe '#item_list' do
+    it 'prints out an item list' do
+      item = double('item', code: 1, name: 'Coca-Cola', price: 100, quantity: 10)
+      items = double('items', list: [item])
+      expect(subject.item_list(items)).to eq 'Code: 01, name: Coca-Cola, price: 100 quantity: 10.'
+    end
+
+
 end
