2002100ef4ee637ab1fe99d9ec37b30cd5d08871
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 07:40:13 +0000

    Item test initiaalization is creeated, fails

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,3 +1,17 @@
-require "item"
-describe Item do 
-end 
+# frozen_string_literal: true
+
+require 'item'
+describe Item do
+  let(:item) { described_class.new(code, name, price, quantity) }
+  let(:code) { 1 }
+  let(:name) { 'Coca Cola' }
+  let(:price) { 1.00 }
+  let(:quantity) { 8 }
+
+  it 'is initialized by name and price' do
+    expect(item.code).to eq 1
+    expect(item.name).to eq name
+    expect(item.price).to eq price
+    expect(item.quantity).to eq quantity
+  end
+end
