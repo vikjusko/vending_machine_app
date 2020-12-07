79b3277c2feea4ad4b475e120b3bfa8b43e74da3
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 07:53:49 +0000

    Coin test added, fails

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -1,3 +1,10 @@
-require "coin"
+# frozen_string_literal: true
+
+require 'coin'
 describe Coin do
+  let(:coin) { described_class.new(20) }
+
+  it 'is initilized by value and quantity' do
+    expect(coin.value).to eq 20
+  end
 end
