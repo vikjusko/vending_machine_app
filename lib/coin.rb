97f0ca8c35e2cfda198074fd5e8f45e9c5429cdb
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 07:54:33 +0000

    Coin class is set up, test passes

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Coin
+  attr_reader :value, :quantity
+
+  VALID_COINS = [200, 100, 50, 20, 10, 5, 2, 1].freeze
+
+  def initialize(value, quantity = 30)
+    @value = value
+    @quantity = quantity
+  end
 end
