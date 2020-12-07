e236af3e853bf9ffa02d2b112c1719f415cc0410
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 08:46:22 +0000

    Trasaction initialization test added, passes

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,4 +3,11 @@ describe Transaction do
 	before(:each) do
   allow($stdout).to receive(:puts)
 end
+
+  let(:purchase) { described_class.new }
+  describe '#initialize' do
+    it 'is initialized with a list of Items that belong to an item class' do
+      expect(purchase.items.list).to be_an Array
+    end
+	end
 end 
