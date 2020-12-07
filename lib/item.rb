d4c2a29070b6556e7fa550cf31124bafa12e67b0
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 07:40:49 +0000

    Item class is set up, test passes

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Item
+
+	attr_reader :code, :name, :quantity, :price
+
+def initialize(code, name, price, quantity)
+  @code = code
+  @name = name
+  @price = price
+  @quantity = quantity
+end
 end
