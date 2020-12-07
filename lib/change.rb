686b1ac46644e8cab00ee3ad531fd2c7a4b7aaa9
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 08:04:15 +0000

    Initial change test is set up, fails

--- lib/change.rb
+++ lib/change.rb
@@ -1,4 +1,11 @@
-require_relative "./coin"
+# frozen_string_literal: true
+
+require_relative './coin'
 
 class Change
+  let(:change) { described_class.new }
+
+  it 'is initialized by coin supply that is an array' do
+    expect(change.coins).to be_an Array
+  end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -1,3 +1,5 @@
-require "change"
-describe Change do 
-end 
+# frozen_string_literal: true
+
+require 'change'
+describe Change do
+end
