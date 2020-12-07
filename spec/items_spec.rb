84de4bc20af16acdda326bd7ca7b37d498fb6d96
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 07:51:24 +0000

    Items class is ready, tests pass

--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -5,5 +5,9 @@ require 'items'
 describe Items do
   it 'is initialized by a list of of items' do
     expect(subject.list).to eq Items::ITEMS
-  end
+	end
+	
+	it "items belong to a class of item" do
+  expect(subject.list[0]).to be_an_instance_of(Item)
+end
 end
