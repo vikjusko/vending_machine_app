3331ab1de99c9c6ee47eddd782bc0345ac9da4d0
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 08:07:02 +0000

    Issue coin test is set up, test fails

--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -7,4 +7,14 @@ describe Change do
   it 'is initialized by coin supply that is an array' do
     expect(change.coins).to be_an Array
   end
+
+  it 'coins belong to a class of coin' do
+    expect(change.coins[0]).to be_an_instance_of(Coin)
+  end
+
+  describe '#issue_coin' do
+    it 'can update the quantity of a certain coin in the machine after giving a coin refund' do
+      expect(change.issue_coin(50, 1)).to eq 29
+    end
+  end
 end
