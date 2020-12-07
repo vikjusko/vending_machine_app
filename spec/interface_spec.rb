72ca3c80fc0a7e6e885d236af0b24a6652b2e806
Victoria Jusko <vikjusko@gmail.com>
2020-12-07 08:17:01 +0000

    Interface welcome test is set up, fails

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,4 +1,13 @@
 require "interface"
 describe Interface do
-	
+	before(:each) do
+  allow($stdout).to receive(:puts)
+end
+
+  describe '#welcome message of the interface' do
+    it 'prints a welcome message when its cllased' do
+      expect(subject.welcome).to eq("Hey there, someone's hungry?!🍫🍿🙈\nGo on then, make your choice!")
+    end
+  end
+
 end
